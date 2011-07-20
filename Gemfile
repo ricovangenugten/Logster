d5c4ab6cca1362ef36b2126bcf1c9594c90c2766
Rico van Genugten <r.vangenugten@viaviela.nl>
2011-07-20 14:54:27 +0200

    initial rails 3.0.9 app including jquery and a few gems

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+gem 'devise'
+gem 'jquery-rails'
+gem 'twitter'
+gem 'rake'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logster::Application.config.secret_token = '6962808478eaa65f92815e9fbe8424e5b8e26c22e9c7c3f091d6d09020d09e09027900410ea3aa10bc2a092125d4a0dffe515b426a08e199ac3e2580fb920516'
